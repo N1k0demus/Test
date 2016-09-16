87d952b64a51c62e3e12e55ea4015396fe01299d
n1k0demus <niklas@minjons.com>
2016-09-16 14:10:58 +0100

    test

---
+++ Testdemo/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testdemo
+{
+    public class Calculator
+    {
+
+        public int Sum(int tal1, int tal2)
+        {
+            return tal1 - tal2;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testdemo;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCalculator()
+        {
+            Calculator calc = new Calculator
+();
+            
+            //arrange
+            int tal1 = 5;
+            int tal2 = 3;
+            int summa = 8;
+            
+            //act
+            int result = calc.Sum(tal1, tal2);
+
+            //assert
+            Assert.AreEqual(summa, result, "Result is not equal to expected value" + result);
+            
+        }
+    }
+}
